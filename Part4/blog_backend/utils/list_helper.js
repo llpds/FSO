951aaa0f77b9e8f5a9351ecb52fb1c0c9403fe7f
llpds <llpds@mail.ru>
2023-08-21 13:08:48 +0300

    4.5*: helper functions and unit tests, step3

--- Part4/blog_backend/utils/list_helper.js
+++ Part4/blog_backend/utils/list_helper.js
@@ -1,8 +1,23 @@
-const dummy = (blogs) => 1
-
+const dummy = (blogs) => {
+  if (blogs) return 1
+}
 const totalLikes = (blogs) => blogs.reduce((acc, val) => acc + val.likes, 0)
+
+//const favoriteBlog = (blogs) => blogs.reduce((acc, val) => acc < val.likes ? val.likes : acc, 0)
+const favoriteBlog = (blogs) => {
+  const favoriteBlog = blogs.reduce((acc, val) => {
+    if(acc.likes < val.likes) return val
+    return acc
+  })
+
+  delete favoriteBlog.__v
+  delete favoriteBlog._id
+  delete favoriteBlog.url
+  return favoriteBlog
+}
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
