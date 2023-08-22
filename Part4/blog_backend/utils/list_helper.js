d3992349ecb8add75e9eec917e281d6c8f79d725
llpds <llpds@mail.ru>
2023-08-22 10:49:30 +0300

    4.6*: helper functions and unit tests, step4

--- Part4/blog_backend/utils/list_helper.js
+++ Part4/blog_backend/utils/list_helper.js
@@ -3,21 +3,34 @@ const dummy = (blogs) => {
 }
 const totalLikes = (blogs) => blogs.reduce((acc, val) => acc + val.likes, 0)
 
-//const favoriteBlog = (blogs) => blogs.reduce((acc, val) => acc < val.likes ? val.likes : acc, 0)
 const favoriteBlog = (blogs) => {
-  const favoriteBlog = blogs.reduce((acc, val) => {
-    if(acc.likes < val.likes) return val
-    return acc
+  const blog = blogs.reduce((acc, val) => acc = acc.likes < val.likes ? val : acc)
+
+  delete blog.__v
+  delete blog._id
+  delete blog.url
+  return blog
+}
+
+const mostBlog = (blogs) => {
+  let authors = {}
+  blogs.forEach(blog => {
+    const name = blog.author
+    if(authors[name]) authors[name] += 1
+    else authors[name] = 1
   })
 
-  delete favoriteBlog.__v
-  delete favoriteBlog._id
-  delete favoriteBlog.url
-  return favoriteBlog
+  const mostBlogArr = Object.entries(authors).reduce((acc,val) => acc = acc[1] < val[1] ? val : acc)
+
+  return {
+    author: mostBlogArr[0],
+    blogs: mostBlogArr[1]
+  }
 }
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlog
 }
