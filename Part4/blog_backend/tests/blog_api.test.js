e3dba7414388529eda23da3e1b6c7c3ee5795167
llpds <llpds@mail.ru>
2023-09-12 00:16:35 +0300

    4.14 Blog list expansions, step2

--- Part4/blog_backend/tests/blog_api.test.js
+++ Part4/blog_backend/tests/blog_api.test.js
@@ -21,25 +21,25 @@ afterAll(async () => {
 
 
 describe('when there is initially some blogs saved', () => {
-  test('notes are returned JSON', async () => {
+  test('blogs are returned JSON Ex 4.8', async () => {
     await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
   },10000)
 
-  test('ALL NOTES are returned', async () => {
+  test('ALL NOTES are returned Ex. 4.8', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body).toHaveLength(helper.initialBlogs.length)
   },10000)
 
-  test('unique identifier is named ID', async () => {
+  test('unique identifier is named ID Ex 4.9', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body[0].id).toBeDefined()
   })
 
   describe('addition of a new blog', () => {
-    test('NEW BLOG can be added', async () => {
+    test('NEW BLOG can be added Ex 4.10', async () => {
       const newBlog =   {
         title:'new test t1',
         author:'new test a1',
@@ -60,7 +60,7 @@ describe('when there is initially some blogs saved', () => {
       expect(urls).toContain('new test u1')
     },10000)
 
-    test('MISSING LIKES property in request, will be equal 0 by default', async () => {
+    test('MISSING LIKES property in request, will be equal 0 by default Ex 4.11', async () => {
       const newBlog =   {
         title:'new test t2',
         author:'new test a2',
@@ -80,7 +80,7 @@ describe('when there is initially some blogs saved', () => {
       expect(newBlogInDb.likes).toEqual(0)
     },10000)
 
-    test('MISSING TITLE OR URL property in request, will cause of response 400', async () => {
+    test('MISSING TITLE OR URL property in request, will cause of response 400 Ex 4.12', async () => {
       const blogWithOutTitle =   {
         author:'new test a3',
         url:'new test u3',
@@ -114,22 +114,109 @@ describe('when there is initially some blogs saved', () => {
       expect(titles).not.toContain(blogWithOutUrl.title)
     },10000)
   })
-})
 
-describe('deletion of a blog', () => {
-  test('succeed with status code 204 if id is valid', async () => {
-    const blogsAtStart = await helper.blogsInDb()
-    const blogToDelete = blogsAtStart[0]
-    await api
-      .delete(`/api/blogs/${blogToDelete.id}`)
-      .expect(204)
+  describe('DELetion of a blog Ex 4.13', () => {
+    test('succeed with status code 204 if id is valid', async () => {
+      const validNonExistingId = await helper.nonExistingId()
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
 
-    const blogsAtEnd = await helper.blogsInDb()
+      await api // valid UNexisting id
+        .delete(`/api/blogs/${validNonExistingId}`)
+        .expect(204)
 
-    expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1) //blogsAtStart instead of initialBlogs as other tests might change the number of blogs.
+      await api // valid existing id
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
 
-    const urls = blogsAtEnd.map(blog => blog.url)
+      const blogsAtEnd = await helper.blogsInDb()
 
-    expect(urls).not.toContain(blogToDelete.url)
-  },10000)
+      expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1)
+
+      const urls = blogsAtEnd.map(blog => blog.url)
+
+      expect(urls).not.toContain(blogToDelete.url)
+    },10000)
+
+    test('fails with statuscode 400 if id is invalid', async () => {
+      const invalidId = '5a3d5da590'
+
+      await api
+        .delete(`/api/blogs/${invalidId}`)
+        .expect(400)
+    })
+
+  })
+
+  describe('viewing a specific blog', () => {
+    test('succeeds with a valid id', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+
+      const blogToView = blogsAtStart[0]
+
+      const resultBlog = await api
+        .get(`/api/blogs/${blogToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      expect(resultBlog.body).toEqual(blogToView)
+    })
+
+    test('fails with statuscode 404 if blog does not exist', async () => {
+      const validNonExistingId = await helper.nonExistingId()
+
+      await api
+        .get(`/api/blogs/${validNonExistingId}`)
+        .expect(404)
+    })
+
+    test('fails with statuscode 400 if id is invalid', async () => {
+      const invalidId = '5a3d5da590'
+
+      await api
+        .get(`/api/blogs/${invalidId}`)
+        .expect(400)
+    })
+  })
+
+  describe('UPDate a specific blog Ex 4.14', () => {
+    test('blog update', async () => {
+      const blogUpdate =   {
+        title:'test title upd',
+        author:'test author upd',
+        url:'test url upd',
+        likes:111
+      }
+
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToUpdate = blogsAtStart[0]
+
+      const resultBlog = await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(blogUpdate)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      delete resultBlog.body.id
+
+      expect(resultBlog.body).toEqual(blogUpdate)
+    })
+
+    test('likes update', async () => {
+      const likesUpdate =   {
+        likes:42
+      }
+
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToUpdate = blogsAtStart[0]
+
+      const resultBlog = await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(likesUpdate)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      expect(resultBlog.body.likes).toEqual(likesUpdate.likes)
+    })
+  })
 })
--- Part4/blog_backend/tests/test_helper.js
+++ Part4/blog_backend/tests/test_helper.js
@@ -13,11 +13,25 @@ const initialBlogs = [
   },
 ]
 
+const nonExistingId = async () => {
+  const blog = new Blog({
+    title: 'toRemove',
+    author: 'toRem',
+    url: 'toRemUrl',
+    likes: 5
+  })
+  await blog.save()
+  //await blog.remove() // for mongoose version starting from 6.12.00 (marked as DEPRECATED), tested for "mongoose": "^6.8.4"
+  await blog.deleteOne() // for mongoose version starting from 7.5.00 (https://mongoosejs.com/docs/api/query.html#Query.prototype.deleteOne()), tested for "mongoose": "^7.4.1"
+
+  return blog._id.toString()
+}
+
 const blogsInDb = async () => {
   const blogs = await Blog.find({})
   return blogs.map(blog => blog.toJSON())
 }
 
 module.exports = {
-  initialBlogs, blogsInDb
+  initialBlogs, nonExistingId, blogsInDb
 }
