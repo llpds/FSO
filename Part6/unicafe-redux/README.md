3aa2b853b4892c1f2c9c42c906e0927824901cf2
llpds <llpds@mail.ru>
2023-12-03 20:19:47 +0200

    6.1: unicafe revisited, step1

---
+++ Part6/unicafe-redux/README.md
@@ -0,0 +1,6 @@
+# FSO part 6
+
+  - Redux
+
+  ### 6.1: unicafe revisited, step1
+    - store, reducer and test for them
---
+++ Part6/unicafe-redux/src/reducer.test.js
@@ -0,0 +1,83 @@
+import deepFreeze from 'deep-freeze'
+import counterReducer from './reducer'
+
+describe('unicafe reducer', () => {
+  const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0
+  }
+  const state = initialState
+
+  test('should return a proper initial state when called with undefined state', () => {
+    const action = {
+      type: 'DO_NOTHING'
+    }
+
+    const newState = counterReducer(undefined, action)
+    expect(newState).toEqual(initialState)
+  })
+
+  test('good is incremented', () => {
+    const action = {
+      type: 'GOOD'
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('ok is incremented ex 6.1', () => {
+    const action = {
+      type: 'OK'
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented ex 6.1', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('reset ex 6.1', () => {
+    const action = {
+      type: 'ZERO'
+    }
+    
+    const notZero = {
+      good: 5,
+      ok: 7,
+      bad: 2
+    }
+
+    deepFreeze(notZero)
+    const newState = counterReducer(notZero, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
+})
