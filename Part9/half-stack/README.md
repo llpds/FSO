65a03e7b4f193c9b3e3597a1efeecd6835ef5c1c
llpds <llpds@mail.ru>
2024-02-24 14:09:44 +0200

    9.15

--- Part9/half-stack/README.md
+++ Part9/half-stack/README.md
@@ -1,5 +1,11 @@
 # FSO Part 9
 
 ### 9.14 new Vite app with TypeScript
+  
   - Simple app shows Head / Course info / total amount of exercises.
       Used components inside App.tsx: Header, Content, Total
+
+### 9.15
+  
+  - Type usege, type narrowing
+      all components are still in the App.tsx
--- Part9/half-stack/src/App.tsx
+++ Part9/half-stack/src/App.tsx
@@ -4,18 +4,87 @@ interface HeaderProps {
 
 const Header = ({ name }: HeaderProps) => <h1>{name}</h1>
 
-interface Course {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
 }
 
-interface ContentProps {
-  courseParts : Course[];
+interface CoursePartBaseDescription extends CoursePartBase {
+  description: string;
 }
 
-const Content = ({ courseParts }:ContentProps) => <ul> 
-  {courseParts.map(c=> <li key={c.name}>{c.name} {c.exerciseCount}</li>)}
-</ul>
+interface CoursePartBasic extends CoursePartBaseDescription {
+  kind: "basic"
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group"
+}
+
+interface CoursePartSpecial extends CoursePartBaseDescription{
+  requirements: string[],
+  kind: "special"
+}
+
+interface CoursePartBackground extends CoursePartBaseDescription {
+  backgroundMaterial: string;
+  kind: "background"
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartSpecial| CoursePartBackground;
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+interface PartProps {
+  part : CoursePart;
+}
+
+const Part = ({ part }:PartProps) => {
+  switch (part.kind){
+    case "basic":
+      return (<p >
+                 <b>{part.name} {part.exerciseCount}</b> <br />
+                 <i>{part.description}</i>
+        </p>
+      );
+    case "group":
+      return(<p>
+                <b>{part.name} {part.exerciseCount}</b> <br /> 
+                <i>project exercises{part.groupProjectCount}</i>
+        </p>
+      );
+    case "background":
+      return (<p>
+                  <b>{part.name} {part.exerciseCount}</b> <br />
+                  <i>{part.description}</i> <br />
+                  Additional material: {part.backgroundMaterial}
+        </p>
+      );
+    case "special":
+      return (<p>
+                  <b>{part.name} {part.exerciseCount}</b> <br />
+                  <i>{part.description}</i> <br /> 
+                  required skills: {part.requirements.join(', ')}
+        </p>
+      );
+    default:
+      return assertNever(part);
+  }
+}      
+
+
+interface ContentProps {
+  courseParts : CoursePart[];
+}
+
+const Content = ({ courseParts }:ContentProps) => <div> 
+  {courseParts.map(p => <Part key={p.name} part = {p} />)}
+</div>
 
 interface TotalProps {
   totalExercises: number;
@@ -25,18 +94,47 @@ const Total = ({ totalExercises }: TotalProps) => <p> Number of exercises {total
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+
+
+  
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
     }
   ];
 
