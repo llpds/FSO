3e4d6604a4e9fe9b1fa3f4b9d4071bf92bc03e1c
llpds <llpds@mail.ru>
2024-06-04 13:05:37 +0300

    ex13.4

--- Part13/README.md
+++ Part13/README.md
@@ -1,11 +1,19 @@
 # [FSO part 13 - Relational databases ](https://fullstackopen.com/en/part13)
 
   app for fly postgres
+
   node-sequelize -  practice via Docker
   blog - exercises for this chapter via Fly
+  
   Synopsis - short overview with tips
   
   - ex 13.1 - 13.3
-      - connection to db (connect.js)
-      - the blogs table CLI creation
-      - print blogs data on the commandline (cli.js)
+    - connection to db (connect.js)
+    - the blogs table CLI creation
+    - print blogs data on the commandline (cli.js)
+
+  - ex 13.4
+    endpoint: api/blogs
+      - GET (blogs list)
+      - POST (add blog)
+      - DELETE (delete blog dy id)
--- Part13/node-sequelize/index.js
+++ Part13/node-sequelize/index.js
@@ -1,22 +1,76 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const {Sequelize, Model, DataTypes} = require('sequelize')
 const express = require('express')
 const app = express()
 
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+class Note extends Model {}
+Note.init({
+  id:{
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  important: {
+    type: DataTypes.BOOLEAN
+  },
+  date: {
+    type: DataTypes.DATE
+  }
+}, {
+  sequelize,
+  underscores: true,
+  timestamps: false,
+  modelName: 'note'
+})
+Note.sync()
+app.use(express.json())
 
-
-app.get('/api/notes', async(req,res) => {
-  const notes = await sequelize.query('SELECT * FROM notes', { type:
-    QueryTypes.SELECT
-  })
-
+app.get('/api/notes', async(req, res) => {
+  const notes = await Note.findAll()
+  console.log('notes', notes.map(n => n.toJSON()))
+  console.log('notes', JSON.stringify(notes))
   res.json(notes)
 })
 
-const PORT = process.env.PORT || 3001
+app.get('/api/notes/:id', async(req, res)=> {
+  const note = await Note.findByPk(req.params.id)
+  if(note) {
+    console.log('note', note.toJSON())
+    res.json(note)
+  } else {
+    res.status(404).end()
+  } 
+})
 
-app.listen(PORT, () => {
+app.post('/api/notes', async (req, res) => {
+  console.log('req.body', req.body)
+  
+  try {
+    const note = await Note.create(req.body)
+    res.json(note)  
+  } catch (e){
+    return res.status(400).json({e})
+  }
+})
+
+app.put('/api/notes/:id', async (req, res) =>{
+  const note = await Note.findByPk(req.params.id)
+  if(note){
+    note.important = req.body.important
+    await note.save()
+    res.json(note)
+  }else{
+    req.status(404).end()
+  }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, ()=> {
   console.log(`Server running on port ${PORT}`)
 })
